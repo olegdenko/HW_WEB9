c846aabee92d030332b8624a7c75cd8438595468
Denko <officedenko@gmail.com>
2023-10-15 15:23:13 +0300

    it_is_alive

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
+import json
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
 
-class QuotesSpider(scrapy.Spider):
+class AuthorsSpider(scrapy.Spider):
     name = 'authors'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
+        quotes = []
+        authors = []
         for quote in response.xpath("/html//div[@class='quote']"):
-            yield {
-                "keywords": quote.xpath("div[@class='tags']/a/text()").extract(),
-                "author": quote.xpath("span/small/text()").extract(),
-                "quote": quote.xpath("span[@class='text']/text()").get()
-            }
+            quote_item = {
+                        "keywords": quote.xpath("div[@class='tags']/a/text()").extract(),
+                        "author": quote.xpath("span/small/text()").extract(),
+                        "quote": quote.xpath("span[@class='text']/text()").get()
+                        }
+            quotes.append(quote_item)
+            author = quote_item["author"]
+            if author not in authors:
+                authors.append(author)
+
         next_link = response.xpath("//li[@class='next']/a/@href").get()
         if next_link:
             yield scrapy.Request(url=self.start_urls[0] + next_link)
 
+        with open('quotes.json', 'w') as quotes_file:
+            json.dump(quotes, quotes_file, indent=2)
 
-# run spider
-process = CrawlerProcess()
-process.crawl(QuotesSpider)
-process.start()
+        with open('authors.json', 'w') as authors_file:
+            json.dump(authors, authors_file, indent=2)
+
+
+
+if __name__ == "__main__":
+    
+    process = CrawlerProcess()
+    process.crawl(AuthorsSpider)
+    process.start()
