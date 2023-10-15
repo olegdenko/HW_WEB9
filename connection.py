e54fe85c70e359a6798d5cced18ebf96a49674e2
Denko <officedenko@gmail.com>
2023-10-15 21:03:36 +0300

    dont_add_dat_local_descr

--- connection.py
+++ connection.py
@@ -4,10 +4,10 @@ from pymongo.server_api import ServerApi
 
 client = MongoClient(
     'mongodb+srv://userweb8:567234@cluster0.vsujqym.mongodb.net/?retryWrites=true&w=majority', server_api=ServerApi('1'))
-db = client['hw_web8']
+db = client['hw_web9']
 
 connect(
-    db='hw_web8',
+    db='hw_web9',
     host='mongodb+srv://userweb8:567234@cluster0.vsujqym.mongodb.net/hw_web8?retryWrites=true&w=majority',
     alias='default',
     server_api=ServerApi('1')
--- main.py
+++ main.py
@@ -8,34 +8,45 @@ class AuthorsSpider(scrapy.Spider):
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com']
 
+    def __init__(self):
+        self.quotes = []
+        self.authors = []
+
     def parse(self, response):
-        quotes = []
-        authors = []
         for quote in response.xpath("/html//div[@class='quote']"):
+            author = quote.xpath("span/small/text()").get()
+            if author not in self.authors:
+                self.authors.append(author)
+
             quote_item = {
-                        "keywords": quote.xpath("div[@class='tags']/a/text()").extract(),
-                        "author": quote.xpath("span/small/text()").extract(),
-                        "quote": quote.xpath("span[@class='text']/text()").get()
-                        }
-            quotes.append(quote_item)
-            author = quote_item["author"]
-            if author not in authors:
-                authors.append(author)
+                "tags": quote.xpath("div[@class='tags']/a/text()").extract(),
+                "author": author,
+                "quote": quote.xpath("span[@class='text']/text()").get()
+            }
+            self.quotes.append(quote_item)
 
         next_link = response.xpath("//li[@class='next']/a/@href").get()
         if next_link:
-            yield scrapy.Request(url=self.start_urls[0] + next_link)
+            yield response.follow(next_link, self.parse)
 
+    def closed(self, reason):
         with open('quotes.json', 'w') as quotes_file:
-            json.dump(quotes, quotes_file, indent=2)
+            json.dump(self.quotes, quotes_file, indent=2)
+
+        authors = []
+        for author in self.authors:
+            authors.append({
+                "fullname": author,
+                "born_date": "",  # Додайте дані, якщо вони доступні
+                "born_location": "",  # Додайте дані, якщо вони доступні
+                "description": ""  # Додайте дані, якщо вони доступні
+            })
 
         with open('authors.json', 'w') as authors_file:
             json.dump(authors, authors_file, indent=2)
 
 
-
 if __name__ == "__main__":
-    
     process = CrawlerProcess()
     process.crawl(AuthorsSpider)
     process.start()
